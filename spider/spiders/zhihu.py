14027ed82a468bf7aa35ac369156c91fd7a82fb6
shadow-zzl <836471210@qq.com>
2019-01-10 16:55:04 +0800

    spider

--- spider/spiders/zhihu.py
+++ spider/spiders/zhihu.py
@@ -1,12 +1,12 @@
-import requests
-
-import time
-from http.cookiejar import LWPCookieJar
-import scrapy
-from selenium import webdriver
-import re
+# -*- coding: utf-8 -*-
 import json
 import os
+from http.cookiejar import LWPCookieJar
+import requests
+import scrapy
+from selenium import webdriver
+from scrapy.http import Request
+from urllib import parse
 
 current_path = os.path.dirname(__file__)
 parent_path = os.path.abspath('..')
@@ -14,38 +14,44 @@ path = os.path.join(os.path.abspath(".."),"data")
 session = requests.session()
 session.cookies = LWPCookieJar(filename='cookies.txt')
 
-class ZhiHuLoginSpider(scrapy.Spider):
-    name = 'zhuhu_login'
-    start_urls =['www.zhihu.com']
+class ZhihuSpider(scrapy.Spider):
+    name = 'zhihu'
+    allowed_domains = ['www.zhihu.com']
+    start_urls = ['https://www.zhihu.com/']
 
     def __init__(self):
         self.agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
         try:
-            self.Cookie = self.load_cookie()
-        except Exception as e:
-            self.Cookie=None
+            self.Cookie = self.load_cookie_dict()
+        except Exception:
+            self.get_cookie()
+            self.Cookie=self.load_cookie_dict()
         self.header = {
-                        "Host":"www.zhihu.com",
-                        "Referer":"https://www.zhihu.com",
-                        "User-Agent":self.agent,
-
-                        "Cookie":self.Cookie
+            "Host":"www.zhihu.com",
+            "Referer":"https://www.zhihu.com",
+            "User-Agent":self.agent
         }
+    def start_requests(self):
+        yield Request("https://www.zhihu.com/",headers=self.header,cookies=self.Cookie,callback=self.parse)
+
     def parse(self, response):
+        print(response.text)
+        pass
+    def parse_detail(self):
         pass
 
     def login(self):
-            self.header['Cookie'] =self.Cookie
-            r = session.get("https://www.zhihu.com",headers = self.header,allow_redirects = False)
-            if r.status_code!=200:
-                print("cookie过期，重新登录")
-                self.get_cookie()
-                self.header['Cookie'] = self.load_cookie()
-                r = session.get("https://www.zhihu.com",headers = self.header)
-            print(r.text)
+        self.header['Cookie'] =self.Cookie
+        r = session.get("https://www.zhihu.com/inbox",headers = self.header,allow_redirects = False)
+        if r.status_code!=200:
+            print("cookie过期，重新登录")
+            self.get_cookie()
+            self.header['Cookie'] = self.load_cookie()
+            r = session.get("https://www.zhihu.com",headers = self.header)
+        print(r)
 
     def get_cookie(self):
-        browser = webdriver.Chrome(executable_path="../driver/chromedriver.exe")
+        browser = webdriver.Chrome()
         browser.get("https://www.zhihu.com/signin")
         browser.find_element_by_name("username").send_keys("18614023195")
         browser.find_element_by_name("password").send_keys("123456abc")
@@ -58,6 +64,7 @@ class ZhiHuLoginSpider(scrapy.Spider):
             f.write(cookies_json)
         browser.close()
 
+
     def load_cookie(self):
         with open('cookies.json','r',encoding='utf-8') as f:
             cookies_json = json.loads(f.read())
@@ -65,10 +72,9 @@ class ZhiHuLoginSpider(scrapy.Spider):
         cookies = ';'.join(cookie for cookie in cookies)
         return cookies
 
-zhihu = ZhiHuLoginSpider()
-zhihu.login()
-
-
-
+    def load_cookie_dict(self):
+        with open('cookies.json','r',encoding='utf-8') as f:
+            cookies_json = json.loads(f.read())
+        return cookies_json
 
 
